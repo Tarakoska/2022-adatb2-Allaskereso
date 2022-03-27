d0306278e2d48725637935d21c1059b4a032f6cf
tothakos <takos2070@gmail.com>
2022-03-27 13:45:07 +0200

    first

--- app.py
+++ app.py
@@ -1,22 +1,66 @@
 import tkinter as tk
 import dao
 
-def hirdetesekShow():
-    result = dao.hirdetesekQuerry()
+def showResult(result):
+    root.text.destroy()
+    i=0
     for r in result:
         root.text = tk.Label()
         root.text["text"] = r
         root.text.grid(row=1+i, column=0)
+        i+=1
 
+def oneletrajzokShow():
+    showResult(dao.oneletrajzokQuerry())
+
+def hirdetesekShow():
+    showResult(dao.hirdetesekQuerry())
+
+def felhasznaloShow():
+    showResult(dao.felhasznaloQuerry())
+
+def munkaAdoShow():
+    showResult(dao.munkaAdoQuerry())
+
+def hirdetesFeladasShow():
+    showResult(dao.hirdetesFeladasQuerry())
+
+def allaskeresoShow():
+    showResult(dao.allaskeresoQuerry())
+
+def birtokolShow():
+    showResult(dao.birtokolQuerry())
+
+def jelentkezesShow():
+    showResult(dao.jelentkezesQuerry())
 
 root = tk.Tk()
 root.title("Állásbörze")
-root.geometry("400x300")
-
-root.hirdetesek = tk.Button(text="Hirdetések lekérdezése", command=hirdetesekShow)
-root.hirdetesek.grid(row=0,column=0)
+root.geometry("800x500")
+root.text = tk.Label()
 
 
+root.btn = tk.Button(text="Oneletrajzok", command=oneletrajzokShow)
+root.btn.grid(row=0,column=0)
 
+root.btn = tk.Button(text="Hirdetesek", command=hirdetesekShow)
+root.btn.grid(row=0,column=1)
 
+root.btn = tk.Button(text="Felhasznalo", command=felhasznaloShow)
+root.btn.grid(row=0,column=2)
+
+root.btn = tk.Button(text="Munkaadó", command=munkaAdoShow)
+root.btn.grid(row=0,column=3)
+
+root.btn = tk.Button(text="HirdetesFeladas", command=hirdetesFeladasShow)
+root.btn.grid(row=0,column=4)
+
+root.btn = tk.Button(text="Allaskereso", command=allaskeresoShow)
+root.btn.grid(row=0,column=5)
+
+root.btn = tk.Button(text="Birtokol", command=birtokolShow)
+root.btn.grid(row=0,column=6)
+
+root.btn = tk.Button(text="Jelentkezes", command=jelentkezesShow)
+root.btn.grid(row=0,column=7)
 root.mainloop()
--- dao.py
+++ dao.py
@@ -1,24 +1,36 @@
 import cx_Oracle
-DB_URL="username/password@localhost"
-
-def hirdetesekQuerry():
+DB_URL="SYSTEM/admin@192.168.0.17"
+def querry(sql):
     try:
         con = cx_Oracle.connect(DB_URL)
-
         cursor = con.cursor()
-
-        cursor.execute('select * from Hirdetesek')
+        cursor.execute(sql)
         rows = cursor.fetchall()
         print(rows)
         return rows
-
     except cx_Oracle.DatabaseError as e:
         print("There is a problem with Oracle", e)
     except Exception as err:
         print("Error:"+str(err))
+    # finally:
+    #     if cursor:
+    #         cursor.close()
+    #     if con:
+    #         con.close()
 
-    finally:
-        if cursor:
-            cursor.close()
-        if con:
-            con.close()
+def oneletrajzokQuerry():
+    querry('select * from Oneletrajzok')
+def hirdetesekQuerry():
+    querry('select * from Hirdetesek')
+def felhasznaloQuerry():
+    querry('select * from Felhasznalo')
+def munkaAdoQuerry():
+    querry('select * from Munkaado')
+def hirdetesFeladasQuerry():
+    querry('select * from HirdetesFeladas')
+def allaskeresoQuerry():
+    querry('select * from Allaskereso')
+def birtokolQuerry():
+    querry('select * from Birtokol')
+def jelentkezesQuerry():
+    querry('select * from Jelentkezes')
